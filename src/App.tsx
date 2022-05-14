88bf761658fb46be4712a4dbd4e00e2bd24f75b4
Erick Sutil <ericksutil@gmail.com>
2022-05-14 17:33:32 -0300

    Initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 
-function App() {
+import FormEmployee from './components/FormEmployee';
+
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Listagem</h2>
+
+      <FormEmployee />
     </div>
   );
 }
---
+++ src/components/FormEmployee/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+function FormEmployee(): JSX.Element {
+  return (
+    <S.Content>
+      <p>Formulário</p>
+    </S.Content>
+  );
+}
+
+export default FormEmployee;
