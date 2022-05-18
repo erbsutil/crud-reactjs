c7e08ccc2fa21a3f7543dd2144c8e01b00681cbb
Erick Sutil <ericksutil@gmail.com>
2022-05-17 23:06:01 -0300

    Init tests

---
+++ client/src/components/ListEmployee/ListEmployee.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import EmployeeProvider from '../../context/EmployeeContext';
+import ListEmployee from '.';
+
+describe('<ListEmployee />', () => {
+  beforeEach(() => {
+    render(
+      <EmployeeProvider>
+        <ListEmployee />
+      </EmployeeProvider>,
+    );
+  });
+
+  it('should render correctly', () => {
+    expect(screen.getByText('Lista de funcionários')).toBeInTheDocument();
+  });
+});
